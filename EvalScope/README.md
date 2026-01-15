93b4922421c8b031762c083145fbe52c3b94d436
sjk0825 <jikyong@yonsei.ac.kr>
2026-01-15 07:06:36 +0000

    implement retrieval evaluation

--- EvalScope/README.md
+++ EvalScope/README.md
@@ -11,11 +11,12 @@ $ (evalscope_env) pip install -r requirements.txt
 ```
 $ (evalscope_env) cd EvalScope
 $ (evalscope_env) python evaluation_mQA.py
+$ (evalscope_env) python evaluation_retrieval.py
 ```
 
 ### View visualization
 ```
-$ (evalscope_env) evalscope app --lang en # 0.0.0.0:7860
+$ (evalscope_env) evalscope app --lang en  --server-port 5000 # 0.0.0.0:7860
 
 ```
 <img width="1462" height="925" alt="image" src="https://github.com/user-attachments/assets/6666e45b-c3b9-4fc6-a2ce-956b379e6068" />
--- EvalScope/evaluation_retrieval.py
+++ EvalScope/evaluation_retrieval.py
@@ -7,44 +7,25 @@ from evalscope import TaskConfig, run_task
 from evalscope.api.model import ModelOutput, GenerateConfig
 from evalscope.models.mockllm import MockLLM
 
-def load_custom_outputs(prediction_path):
-    outputs = []
-    with open(prediction_path) as f:
-        for line in f:
-            obj = json.loads(line)
-            outputs.append(
-                ModelOutput.from_content(
-                    model="mockllm",
-                    content=obj["prediction"]
-                )
-            )
-    return outputs
 
 def main(config):
 
-    task_config = config["task_config"]
-    prediction_path = config["prediction_path"]
-
-    custom_outputs = load_custom_outputs(prediction_path)
-
-    mock_model = MockLLM(
-        model_name="mockllm",
-        custom_outputs=custom_outputs,
-        config=GenerateConfig()
-    )
-
-    task_cfg = TaskConfig(
-        model=mock_model,
-        datasets=task_config["datasets"],
-        dataset_args=task_config["dataset_args"],
-    )
+    task_cfg = {
+        "work_dir": config["work_dir"],
+        "eval_backend": config["eval_backend"],
+        "eval_config": {
+            "tool": config["eval_config"]["tool"],
+            "model": config["eval_config"]["model"],
+            "eval": config["eval_config"]["eval"]
+        },
+    }
     run_task(task_cfg=task_cfg)
 
     print(task_cfg)
 
 
 if __name__ == '__main__':
-    CONFIG_PATH='config_evalscope_mqa.yml'
+    CONFIG_PATH='config_evalscope_retrieval.yml'
 
     parser = argparse.ArgumentParser(description="Load and print YAML configuration.")
     parser.add_argument('--config-path', type=str, default=CONFIG_PATH, help='Path to the YAML configuration file')
